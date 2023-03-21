8f2fa33deaed0aa7d5bd29d9f59998148db48a4b
akmhmgc <akimuhamaguchi@icloud.com>
2023-03-21 18:15:41 +0900

    feat: インデックスファイルをアップロードする機能を追加

--- chat_github.py
+++ chat_github.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import re
 import streamlit as st
 from llama_index import GPTSimpleVectorIndex, download_loader
 from llama_index.readers.llamahub_modules.github_repo import GithubClient, GithubRepositoryReader
@@ -10,12 +11,13 @@ st.write("Enter your OpenAI API key and GitHub token to start.")
 openai_api_key = st.text_input("OpenAI API Key", value="", type="password")
 github_token = st.text_input("GitHub Token", value="", type="password")
 
+index = None
+
 if openai_api_key and github_token:
     st.write("API keys have been set.")
     
-    # Add input fields for repository information
-    owner = st.text_input("Owner (required)", value="")
-    repo = st.text_input("Repo (required)", value="")
+    # Add input field for repository URL
+    repo_url = st.text_input("Repository URL (required)", value="")
     filter_directories = st.text_input("Filter Directories (optional, comma-separated)", value="")
     filter_file_extensions = st.text_input("Filter File Extensions (optional, comma-separated)", value="")
 
@@ -23,7 +25,21 @@ if openai_api_key and github_token:
     filter_directories = tuple(filter_directories.split(',')) if filter_directories else None
     filter_file_extensions = tuple(filter_file_extensions.split(',')) if filter_file_extensions else None
 
-    if st.button("Make index data"):
+    # Parse owner and repo from the URL
+    url_pattern = re.compile(r"(?:https?:\/\/)?(?:www\.)?github\.com\/([^\/]+)\/([^\/]+)")
+    match = url_pattern.match(repo_url)
+    owner, repo = match.groups() if match else (None, None)
+
+    uploaded_file = st.file_uploader("Upload index file (Optional)")
+
+    if uploaded_file:
+        with open("uploaded_docs.pkl", "wb") as f:
+            f.write(uploaded_file.getvalue())
+        with open("uploaded_docs.pkl", "rb") as f:
+            docs = pickle.load(f)
+        index = GPTSimpleVectorIndex(docs)
+
+    if st.button("Make index data") and owner and repo and not index:
         # Show a loading message while the data is being created
         with st.spinner("Loading data..."):
             # Load the data
@@ -48,6 +64,17 @@ if openai_api_key and github_token:
 
             index = GPTSimpleVectorIndex(docs)
 
+        if st.button("Download index data"):
+            with open("docs.pkl", "rb") as f:
+                tmp = f.read()
+            st.download_button(
+                label="Download index data",
+                data=tmp,
+                file_name="docs.pkl",
+                mime="application/octet-stream",
+            )
+
+    if index:
         # Show the question input field after the data is loaded
         user_question = st.text_input("Enter your question:", value="")
         if user_question:
